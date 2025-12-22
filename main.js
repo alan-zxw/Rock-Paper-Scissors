9417d50caa2de02f1c54f10602b7b6a6ba156363
alan-zxw <244425592+alan-zxw@users.noreply.github.com>
2025-12-22 17:12:08 -0500

    Function playGame can play 5 rounds of game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Hello world!")
+
 
 let humanScore = 0
 let computerScore = 0
@@ -25,26 +25,34 @@ function playRound(humanChoice, computerChoice) {
     console.log("------------------------------------")
     if(humanChoice.toUpperCase() == computerChoice) {
         console.log("It's a tie!")
+        humanScore += 1
+        computerScore += 1
     }
     else if(humanChoice.toUpperCase() == "ROCK" && computerChoice == "SCISSORS") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou win!")
+        humanScore += 1
     }
     else if(humanChoice.toUpperCase() == "ROCK" && computerChoice == "PAPER") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou lose!")
+        computerScore += 1
     }
 
     else if(humanChoice.toUpperCase() == "PAPER" && computerChoice == "SCISSORS") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou lose!")
+        computerScore += 1
     }
     else if(humanChoice.toUpperCase() == "PAPER" && computerChoice == "ROCK") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou win!")
+        humanScore += 1
     }
 
     else if(humanChoice.toUpperCase() == "SCISSORS" && computerChoice == "PAPER") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou win!")
+        humanScore += 1
     }
     else if(humanChoice.toUpperCase() == "SCISSORS" && computerChoice == "ROCK") {
         console.log("You played: " + humanChoice + "\nComputer played: " + computerChoice + "\nYou lose!")
+        computerScore += 1
     }
     else {
         console.log("Error occured. . .")
@@ -54,5 +62,11 @@ function playRound(humanChoice, computerChoice) {
     console.log("------------------------------------")
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+}
+
+
+playGame()
